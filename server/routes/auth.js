0ae11a3efa29d834a25715ea8753046936408fb0
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-10-03 18:00:03 -0400

    addin expreess-validation middlewares

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {userRegisterValidation} = require('../validators/auth');
+const {runValidation} = require('../validators/index');
 
 
 const {register} = require('../controller/controller.js')
 
-router.use('/register',register )
+router.use('/register', userRegisterValidation, runValidation ,register );
 
 
 module.exports = router;
---
+++ server/validators/auth.js
@@ -0,0 +1,15 @@
+const {check}  = require('express-validator');
+exports.userRegisterValidation = [
+    check('name')
+        .not()
+            .isEmpty()      
+                .withMessage('Name is required'),
+    check('email')
+        .not()
+            .isEmail()      
+                .withMessage('Must be a valid email address'),
+    check('password')
+        .isLength({min:8})   
+                .withMessage('Password Must Be 8 Character Long')
+
+]
