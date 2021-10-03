9a5d7b20def56b090916ecf387eb5f5fcf2ed445
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-10-03 19:34:43 -0400

    addin aws access keys and generating ses

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,3 +1,40 @@
+const AWS = require('aws-sdk');
+AWS.config.update({
+    region:'us-west-2',
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
+    
+});
+const ses=  new AWS.SES({apiVersion:'2010-12-01'});
 exports.register = (req, res) => {
-        console.log('Register controller', req.body)
+    const {user,email,password} = req.body;
+    const params = {
+        Source:process.env.EMAIL_FROM,
+        Destination:{
+            ToAddresses:[email]
+        },
+        ReplyToAddresses:[process.env.EMAIL_TO],
+        Message:{
+            Body:{
+                Html:{
+                    Charset:'UTF-8',
+                    Data:`<html><body> <h1>Hello ${user}</h1></body></html>`
+                }
+            },
+            Subject:{
+                Charset:'UTF-8',
+                Data:'Complete Your Registeration'
+            }
+        }
+    }
+
+
+    const sendEmailOnRegister = ()=> ses.sendEmail(params).promise()
+
+    sendEmailOnRegister().then(data =>{
+        console.log(data)
+    }).catch(err => {
+        console.log(err);
+        res.json('Sendind Email Failed');
+    })
     };
