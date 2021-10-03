3ee1129b1860e8a69881774990e5b1019db98fb8
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-10-02 23:53:29 -0400

    addin envs and packages

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express  = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 const authRouters = require('./routes/auth');
 
 
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+app.use(cors())
 app.use('/api',authRouters);
 
-const port = 8080;
+const port = process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log(`Running on port ${port}`)
 })
