4b94e1779ef0a1e54ef2f17dce9c33a22a720d16
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-10-03 18:19:18 -0400

    setting up mongodb Atlas

--- server/server.js
+++ server/server.js
@@ -1,22 +1,19 @@
-const express  = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
 const authRouters = require('./routes/auth');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express  = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-
-
+app.use(bodyParser.json()); //currently depreceated
+mongoose.connect(process.env.MONGO_DB_CLOUD,{useNewUrlParser:true, useUnifiedTopology:true}).then( () => console.log('Connect to DB')).catch( (err) => console.log('Error'+err));
 app.use(cors({
     origin: '*'
 }));
 
 app.use('/api',authRouters);
-
 
 const port = 8000;
 app.listen(port,()=>{
