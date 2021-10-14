0fadd87abf5e0ee116819f59c145496380d272bf
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-10-14 16:41:02 -0400

    makin req to cookies & api

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,12 +1,26 @@
 import Layout from "../../components/Layout";
 import axios from "axios";
+import {API} from '../../config';
+import {getCookie} from '../../helpers/auth';
 
-const User = ({todos}) => <Layout>{JSON.stringify(todos)}</Layout>
+const User = ({user}) => <Layout>{JSON.stringify(user)}</Layout>
 
 User.getInitialProps = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-    return {
-        todos : response.data
+
+    const token = getCookie('token');
+    try {
+        const response = await axios.get(`${API}/user`,{
+            headers:{
+                authorization: `Bearer ${token}`,
+                contentType: 'application/json'
+            }
+        });
+        return{user: response.data}
+
+    } catch (error) {
+        if(error.response.status === 401){
+            return {user: 'No User'}
+        }
     }
 };
 
