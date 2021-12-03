21a8939d83f90114b7612178b32fde988ef9d79e
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-12-02 22:47:29 -0500

    addin submit link form

--- client/config.js
+++ client/config.js
@@ -1,6 +1,5 @@
 import getConfig from 'next/config' 
 const {publicRuntimeConfig} = getConfig();
-
 export const API = publicRuntimeConfig.API
 export const APP_NAME = publicRuntimeConfig.APP_NAME
 export const DOMAIN = publicRuntimeConfig.DOMAIN
--- client/pages/user/link/create.js
+++ client/pages/user/link/create.js
@@ -1,3 +1,83 @@
-const Create = () => <p>Link Working</p>
+import Layout from '../../../components/Layout'
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import {API} from '../../../config';
+import {showSuccessMessage, showErrorMessage} from '../../../helpers/alert';
+const Create = () =>{
+    const [state, setState] = useState({
+        title: '',
+        url: '',
+        categories: [],
+        loadedCategories:[],
+        success:'',
+        error:'',
+        medium:''
+    });
+
+    const {title, url, categories, loadedCategories, success, error, medium} = state;
+
+    useEffect(()=>{
+        loadCategories();
+
+    },[success]);
+
+    const loadCategories = async () =>{
+        try{
+            const res = await axios.get(`${API}/categories`);
+            setState({...state, loadedCategories:res.data});
+        }catch(err){
+            console.log(err);
+        }
+    };
+
+    const handleChange = name => event =>{}
+
+    const handleSubmit = async event =>{
+
+    }
+
+    const handleTitleChange = event =>{
+        setState({...state, title:event.target.value});
+    }
+
+    const handleURLChange = event =>{
+        setState({...state, url:event.target.value});
+    }
+
+    const submitLinkForm = () =>(
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="text-muted">Title</label>
+                <input type="text" className="form-control" onChange={handleTitleChange} value={title}/>
+            </div>
+            <div className="form-group">
+                <label className="text-muted">URL</label>
+                <input type="url" className="form-control" onChange={handleURLChange} value={url}/>
+            </div>
+            <div>
+                <button className="btn btn-outline-warning" type="submit">Submit</button>
+            </div>
+        </form>
+    );
+
+    return(
+        <Layout>
+            <div className="row">
+                <div className="col-md-12">
+                <h1>Submit Link</h1>
+                <br />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-4">
+                    xxxx
+                </div>
+                <div className="col-md-8">
+                    {submitLinkForm()}
+                </div>
+            </div>
+        </Layout>
+    );
+}
 
 export default Create;
